249b1f18989197a9d3f68ccc7c65957bf4ae7b03
PutraRaimarDelvin <delvinputra805@gmail.com>
2024-11-21 18:33:20 +0700

    Menambahkan data class mahasiswa dengan parameternya

---
+++ app/src/main/java/com/example/navigasi/model/Mahasiswa.kt
@@ -0,0 +1,10 @@
+package com.example.navigasi.model
+
+data class Mahasiswa(
+    val nama: String = "",
+    val gender: String = "",
+    val alamat: String = "",
+    val email: String = "",
+    val nohp: String = "",
+
+)
