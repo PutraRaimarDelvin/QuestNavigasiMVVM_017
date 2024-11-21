2af1bd8a4260300685a59121d92398aeb59150bd
PutraRaimarDelvin <delvinputra805@gmail.com>
2024-11-21 19:00:16 +0700

    membuat enum class Halaman

---
+++ app/src/main/java/com/example/navigasi/ui/viewmodel/Navigasi.kt
@@ -0,0 +1,26 @@
+package com.example.navigasi.ui.viewmodel
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigasi.model.ListGender
+import com.example.navigasi.ui.view.DetailMahasiswaView
+import com.example.navigasi.ui.view.FormulirMahasiswaView
+
+
+enum class Halaman{
+    Form,
+    Data
+}
+
+
+
